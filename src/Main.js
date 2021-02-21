16d2f39cfb68a31d494f5a81c703a304dbffb05e
Collins Muriuki <murerwacollins@gmail.com>
2021-02-21 16:18:48 +0300

    Applying stylesheet; learning on the job

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {
+    Text,
+    View,
+    Image,
+    ActivityIndicator,
+    Button,
+    StyleSheet
+} from 'react-native'
 
 const Main = () => {
     return (
-        <View
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-            <Text>Hello world</Text>
+        <View style={styles.container}>
+            <Text style={styles.h1}>Jots</Text>
+            <Text style={styles.paragraph}>This is my app</Text>
+            <ActivityIndicator size="large" />
+            <Button title="Press Me!" />
+            <Image source={require('../assets/images/hello-world.jpg')} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    h1: {
+        fontSize: 48,
+        fontWeight: 'bold'
+    },
+    paragraph: {
+        marginTop: 24,
+        marginBottom: 24,
+        fontSize: 18
+    }
+})
 
 export default Main
