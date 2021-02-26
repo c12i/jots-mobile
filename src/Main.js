3c1ebcd8129fe0332ef8ad24029d86748e4489a5
Collins Muriuki <murerwacollins@gmail.com>
2021-02-26 16:55:11 +0300

    Add status bar

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { ApolloProvider } from '@apollo/client'
 
 import client from './gql/client'
@@ -7,6 +8,7 @@ import Screens from './screens'
 const Main = () => {
     return (
         <ApolloProvider client={client}>
+            <StatusBar hidden={false} barStyle="dark-content" />
             <Screens />
         </ApolloProvider>
     )
